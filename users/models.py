b2746ee5f0336f722f76cfabb8bd86823d95eec7
wookkl <wjddnr315@gmail.com>
2020-10-02 17:46:58 +0900

    add User models fields

--- users/models.py
+++ users/models.py
@@ -3,4 +3,20 @@ from django.db import models
 
 # Create your models here.
 class User(AbstractUser):
-    pass
+    """ Custom User Model """
+
+    GENDER_MALE = "male"
+    GENDER_FEMALE = "female"
+    GENDER_OTHER = "other"
+
+    GENDER_CHOICES = (
+        (GENDER_MALE, "Male"),
+        (GENDER_FEMALE, "Female"),
+        (GENDER_OTHER, "Other"),
+    )
+
+    avater = models.ImageField(null=True, blank=True)
+    gender = models.CharField(
+        choices=GENDER_CHOICES, max_length=10, null=True, blank=True
+    )
+    bio = models.TextField(default="", blank=True)
