f0cafc917f74223ca815e17b3f8802ae9a7f9851
wookkl <wjddnr315@gmail.com>
2020-10-04 14:43:03 +0900

    migrate room model

--- rooms/models.py
+++ rooms/models.py
@@ -4,6 +4,23 @@ from core import models as core_models
 from users import models as user_models
 
 
+class AbstractItem(core_models.AbstractTimeStamp):
+
+    """ AbstractItem """
+
+    name = models.CharField(max_length=80)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.name
+
+
+class RoomType(AbstractItem):
+    pass
+
+
 class Room(core_models.AbstractTimeStamp):
 
     """ Room Model Definifion """
@@ -22,3 +39,7 @@ class Room(core_models.AbstractTimeStamp):
     cehck_out = models.TimeField()
     instant_book = models.BooleanField(default=False)
     host = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
+    room_type = models.ManyToManyField(RoomType, blank=True)
+
+    def __str__(self):
+        return self.name
