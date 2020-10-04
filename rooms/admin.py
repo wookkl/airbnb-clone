72a90110f529a7a3612ce0cbfb90f0012d47c6c7
wookkl <wjddnr315@gmail.com>
2020-10-04 16:45:31 +0900

    Add RoomType, Amenity, Facility, HouseRule, Photo

--- rooms/admin.py
+++ rooms/admin.py
@@ -2,11 +2,25 @@ from django.contrib import admin
 from . import models
 
 
-@admin.register(models.RoomType)
-class RoomTypeAdmin(admin.ModelAdmin):
+@admin.register(models.RoomType, models.Amenity, models.Facility, models.HouseRule)
+class ItemAdmin(admin.ModelAdmin):
+
+    """ ItemAdmin Definition """
+
     pass
 
 
 @admin.register(models.Room)
 class RoomAdmin(admin.ModelAdmin):
+
+    """ Romm Admin Definition """
+
+    pass
+
+
+@admin.register(models.Photo)
+class PhotoAdmin(admin.ModelAdmin):
+
+    """ PhotoAdmin Definition """
+
     pass
