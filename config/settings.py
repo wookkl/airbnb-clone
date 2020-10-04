6e6a751d4cb290f81ce66848f193e07d068fe99a
wookkl <wjddnr315@gmail.com>
2020-10-04 12:58:50 +0900

    clean user model and migrate just once

--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,9 @@ DJANGO_APPS = [
     "django.contrib.staticfiles",
 ]
 PROJECT_APPS = ["users.apps.UsersConfig"]
+
+THIRD_PARTY_APPS = []
+
 INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS
 
 MIDDLEWARE = [
