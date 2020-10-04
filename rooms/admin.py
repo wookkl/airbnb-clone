5b96d94c7d4ef276b4c961c251d713b35c29e1f2
wookkl <wjddnr315@gmail.com>
2020-10-04 14:53:55 +0900

    Add roomtype model

--- rooms/admin.py
+++ rooms/admin.py
@@ -2,7 +2,11 @@ from django.contrib import admin
 from . import models
 
 
+@admin.register(models.RoomType)
+class RoomTypeAdmin(admin.ModelAdmin):
+    pass
+
+
 @admin.register(models.Room)
 class RoomAdmin(admin.ModelAdmin):
-
     pass
