e54a668964f94ad88c25c9a49c82edcd70b54405
wookkl <wjddnr315@gmail.com>
2020-10-06 17:45:31 +0900

    Set review model and create review model

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from core import models as core_models
 
-# Create your models here.
+
+class Review(core_models.AbstractTimeStamp):
+
+    """ Review Model Definition """
