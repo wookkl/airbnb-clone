26610dc43b880444b3b8cb35efaba498169a730e
wookkl <wjddnr315@gmail.com>
2020-10-06 18:29:41 +0900

    Migrate review app model

--- reviews/models.py
+++ reviews/models.py
@@ -5,3 +5,16 @@ from core import models as core_models
 class Review(core_models.AbstractTimeStamp):
 
     """ Review Model Definition """
+
+    review = models.TextField()
+    accuracy = models.IntegerField()
+    communication = models.IntegerField()
+    cleaniness = models.IntegerField()
+    location = models.IntegerField()
+    check_in = models.IntegerField()
+    value = models.IntegerField()
+    user = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    room = models.ForeignKey("rooms.Room", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.review
