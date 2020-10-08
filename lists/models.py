1fb8123fdf87c5763c5fb38c86c2cd3ba5dc2aeb
wookkl <wjddnr315@gmail.com>
2020-10-08 17:17:58 +0900

    add related_name and migrate

--- lists/models.py
+++ lists/models.py
@@ -5,8 +5,10 @@ from core import models as core_models
 class List(core_models.AbstractTimeStamp):
 
     name = models.CharField(max_length=80)
-    user = models.ForeignKey("users.user", on_delete=models.CASCADE)
-    rooms = models.ManyToManyField("rooms.Room", blank=True)
+    user = models.ForeignKey(
+        "users.user", related_name="lists", on_delete=models.CASCADE
+    )
+    rooms = models.ManyToManyField("rooms.Room", related_name="lists", blank=True)
 
     def __str__(self):
         return self.name
